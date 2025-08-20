1e931f880df9bb3cf37799a9f0c1bff738d500d1
Pratik-hitech <pratik@hitechvalley.com.au>
2025-08-20 13:16:06 +0545

    manuals

--- src/components/ErrorHandlers/ErrorBoundary.jsx
+++ src/components/ErrorHandlers/ErrorBoundary.jsx
@@ -1,36 +1,45 @@
-// src/components/ErrorBoundary.jsx
-import React from "react";
-import { FallbackProps } from "react-error-boundary";
-import { Box, Typography, Button } from "@mui/material";
+// // src/components/ErrorBoundary.jsx
+// import React from "react";
+// import { FallbackProps } from "react-error-boundary";
+// import { Box, Typography, Button } from "@mui/material";
 
-const ErrorFallback = ({ error, resetErrorBoundary }) => {
+// const ErrorFallback = ({ error, resetErrorBoundary }) => {
+//   return (
+//     <Box
+//       sx={{
+//         display: "flex",
+//         flexDirection: "column",
+//         alignItems: "center",
+//         justifyContent: "center",
+//         height: "100vh",
+//         textAlign: "center",
+//         p: 3,
+//       }}
+//     >
+//       <Typography variant="h4" color="error" gutterBottom>
+//         Something went wrong!
+//       </Typography>
+//       <Typography variant="body1" sx={{ mb: 2 }}>
+//         {error.message}
+//       </Typography>
+//       <Button
+//         variant="contained"
+//         color="primary"
+//         onClick={resetErrorBoundary}
+//       >
+//         Try Again
+//       </Button>
+//     </Box>
+//   );
+// };
+
+// export default ErrorFallback;
+import React from 'react'
+
+const ErrorBoundary = () => {
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "100vh",
-        textAlign: "center",
-        p: 3,
-      }}
-    >
-      <Typography variant="h4" color="error" gutterBottom>
-        Something went wrong!
-      </Typography>
-      <Typography variant="body1" sx={{ mb: 2 }}>
-        {error.message}
-      </Typography>
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={resetErrorBoundary}
-      >
-        Try Again
-      </Button>
-    </Box>
-  );
-};
+    <div>ErrorBoundary</div>
+  )
+}
 
-export default ErrorFallback;
+export default ErrorBoundary
